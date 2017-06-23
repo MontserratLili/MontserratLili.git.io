f2bb09ace5d47bf6b6231ce668599fa73d526637
MontserratLili <lauraliligm@gmail.com>
2017-06-23 14:02:21 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+var irIntereses = document.getElementById("mas");
+
+function mostrarIntereses(){
+    document.getElementById("intereses").style.display = "inline-block";
+};
+
+irIntereses.addEventListener("click", mostrarIntereses);
+
