80e2d907bfe78285cdd191f9016e5669455434b4
MontserratLili <lauraliligm@gmail.com>
2017-08-07 16:46:47 -0500

    agregando habilidades y proyectos

--- js/app.js
+++ js/app.js
@@ -3,5 +3,5 @@ $(document).ready(function () {
         document.getElementById("bienvenido").style.display = "none";
         location.href = ("#home");
     }, 3000);
-    $('.carousel.carousel-slider').carousel({fullWidth: true}); 
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
 });
